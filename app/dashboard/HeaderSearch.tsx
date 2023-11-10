066ba7130bf83160c0544d8ae06d66dd8495588c
Stanislav <verbatim8872@gmail.com>
2023-11-11 00:33:43 +0200

    rm portal from HeaderSearch modal

--- app/dashboard/HeaderSearch.tsx
+++ app/dashboard/HeaderSearch.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useRef, useState } from "react";
-import { createPortal } from "react-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { CSSTransition } from "react-transition-group";
 
@@ -26,37 +25,32 @@ export default function HeaderSearch() {
         unmountOnExit
         nodeRef={nodeRef}
       >
-        <>
-          {createPortal(
+        <div
+          className="modal-container absolute bottom-0 left-0 right-0 top-0 z-50 flex items-start justify-center bg-[#07165871] backdrop-blur-sm transition-all "
+          onClick={() => setSearchModalVisible(false)}
+          ref={nodeRef}
+        >
+          <div
+            className="modal-window relative mt-20 w-[100%] max-w-[480px] overflow-hidden rounded-lg border-transparent bg-white outline-white"
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            <input
+              ref={inputRef}
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+              className="h-[58px] w-full rounded-lg bg-white px-4 pr-[40px] placeholder-[#aeaeae] outline-[#fff]"
+              placeholder="Find anything..."
+            />
             <div
-              className="modal-container absolute bottom-0 left-0 right-0 top-0 z-50 flex items-start justify-center bg-[#07165871] backdrop-blur-sm transition-all "
-              onClick={() => setSearchModalVisible(false)}
-              ref={nodeRef}
+              onClick={() => inputRef.current?.focus()}
+              className="absolute bottom-0 right-0 top-0 flex w-[40px] cursor-text items-center justify-center"
             >
-              <div
-                className="modal-window relative mt-20 w-[100%] max-w-[480px] overflow-hidden rounded-lg border-transparent bg-white outline-white"
-                onClick={(e) => {
-                  e.stopPropagation();
-                }}
-              >
-                <input
-                  ref={inputRef}
-                  value={searchValue}
-                  onChange={(e) => setSearchValue(e.target.value)}
-                  className="h-[58px] w-full rounded-lg bg-white px-4 pr-[40px] placeholder-[#aeaeae] outline-[#fff]"
-                  placeholder="Find anything..."
-                />
-                <div
-                  onClick={() => inputRef.current?.focus()}
-                  className="absolute bottom-0 right-0 top-0 flex w-[40px] cursor-text items-center justify-center"
-                >
-                  <AiOutlineSearch size={20} color="#aeaeae" />
-                </div>
-              </div>
-            </div>,
-            document.body,
-          )}
-        </>
+              <AiOutlineSearch size={20} color="#aeaeae" />
+            </div>
+          </div>
+        </div>
       </CSSTransition>
     </>
   );
