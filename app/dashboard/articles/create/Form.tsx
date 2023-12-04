03b9f955488329464cedaee077207f079e037de8
Stanislav <verbatim8872@gmail.com>
2023-12-04 23:44:22 +0200

    onPickImage close modal

--- app/dashboard/articles/create/Form.tsx
+++ app/dashboard/articles/create/Form.tsx
@@ -28,6 +28,7 @@ export default function Form() {
 
   const onPickImage = () => {
     setPicture(imagePickerState.uploadedImg);
+    imagePickerState.setVisible(false);
   };
 
   const onChange = (e: FormEvent<HTMLFormElement>) => {
