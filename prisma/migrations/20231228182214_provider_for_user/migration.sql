58a968a59203c6c4cf615e2df853dee15f22856e
Stanislav <verbatim8872@gmail.com>
2023-12-28 20:23:05 +0200

    add provider for User DB

---
+++ prisma/migrations/20231228182214_provider_for_user/migration.sql
@@ -0,0 +1,5 @@
+-- CreateEnum
+CREATE TYPE "UserProvider" AS ENUM ('CREDENTIALS', 'GOOGLE');
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "provider" "UserProvider" NOT NULL DEFAULT 'CREDENTIALS';
